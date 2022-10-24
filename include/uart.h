a5a3becac5cad43c145e5cb779ac2038f38fd40c
Sam Skinner <30613658+sskii@users.noreply.github.com>
2022-10-24 20:37:13 +1300

    Team 10's tweaks (#1)
    * Separated debugging into own environment
    
    * Force all compiler warnings
    
    * Add docs
    
    * nitpick formatting, add clang-format
    
    Co-authored-by: David Cole <david.cole1340@gmail.com>

--- include/uart.h
+++ include/uart.h
@@ -1,12 +1,12 @@
 #ifndef UART_H
 #define UART_H
 
-#include <stdint.h>
 #include <avr/io.h>
+#include <stdint.h>
 
 /**
  * @brief initialise the UART peripheral.
- * 
+ *
  * @param baud baud rate to use.
  */
 void uart_init(uint32_t baud);
@@ -14,7 +14,7 @@ void uart_init(uint32_t baud);
 /**
  * @brief transmit a byte of data over UART, blocking until the data has
  * transferred.
- * 
+ *
  * @param data data to transfer.
  */
 void uart_transmit(uint8_t data);
--- src/uart.c
+++ src/uart.c
@@ -1,14 +1,26 @@
 #include "uart.h"
+#include "common.h"
 
 #include <avr/io.h>
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * @brief print a single character over UART
+ *
+ * @param c character to print
+ * @param stream unused
+ * @return int
+ */
 static int uart_debug_printf(char c, FILE *stream) {
+  UNUSED_PARAM(stream);
   uart_transmit((uint8_t)c);
   return 0;
 }
 
+/**
+ * @brief stdout handle for UART
+ */
 static FILE uart_stdout =
     FDEV_SETUP_STREAM(uart_debug_printf, NULL, _FDEV_SETUP_WRITE);
 
